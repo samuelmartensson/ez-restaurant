f3d0dbe2face7d3c776491a001a709578251ba04
Samuel Mårtensson <samuel137@live.se>
2024-12-12 23:32:54 +0100

    subdomain setup

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,11 +1,28 @@
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
+export const CUSTOMER_ID_HEADER = "x-customer-id";
+
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  const customerId = request.nextUrl.searchParams.get("customer") ?? "";
   const headers = new Headers(request.headers);
-  headers.set("x-customer-id", customerId);
+  const url = new URL(request.url);
+  const hostname = url.hostname;
+  const parts = hostname.split(".");
 
+  if (hostname === "localhost") {
+    headers.set(CUSTOMER_ID_HEADER, "test");
+    return NextResponse.next({ headers });
+  }
+
+  let customerId = "";
+
+  if (parts.length > 2) {
+    customerId = parts[0];
+  } else {
+    customerId = hostname;
+  }
+
+  headers.set(CUSTOMER_ID_HEADER, customerId);
   return NextResponse.next({ headers });
 }
