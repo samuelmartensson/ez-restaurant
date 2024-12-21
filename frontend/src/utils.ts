334cdccee6c72509b317bd2825c73c5aa1529d7d
Samuel Mårtensson <samuel137@live.se>
2024-12-21 02:13:01 +0100

    env url

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,4 +1,5 @@
+const base = process.env.NEXT_PUBLIC_BACKEND_API_URL;
 export const getURL = (key: string, endpoint: string) => {
-  const base = "http://localhost:5232/customer/";
-  return `${base}${endpoint}?key=${key}`;
+  const url = `${base}/customer/`;
+  return `${url}${endpoint}?key=${key}`;
 };
