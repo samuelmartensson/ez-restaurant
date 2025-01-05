9ae142aa1dbec24ec308882341ec868802754deb
Samuel Mårtensson <samuel137@live.se>
2025-01-05 01:39:56 +0100

    update loader

--- admin/src/components/AppLoader.tsx
+++ admin/src/components/AppLoader.tsx
@@ -1,12 +1,58 @@
-import { LoaderPinwheel } from "lucide-react";
-import React from "react";
+"use client";
+
+import { Coffee } from "lucide-react";
+import { useEffect, useState } from "react";
 
 const AppLoader = () => {
+  const [loadingPhrase, setLoadingPhrase] = useState("");
+
+  function getRandomLoadingPhrase() {
+    const phrases = [
+      "Setting things up",
+      "Almost there",
+      "Loading your experience",
+      "Please wait...",
+      "Gathering data",
+      "Crunching numbers",
+      "Preparing everything",
+      "Just a moment...",
+      "Hang tight",
+      "Getting things ready",
+      "We’re on it",
+      "Making things happen",
+      "Working on it",
+      "Taking care of it",
+      "Loading the magic",
+      "Unpacking your content",
+      "Fetching results",
+      "Building your experience",
+      "Hold tight",
+      "Initializing...",
+      "Please be patient",
+      "Finishing up",
+      "Bringing it all together",
+      "Your content is almost ready",
+      "Just a few more seconds",
+    ];
+
+    const randomIndex = Math.floor(Math.random() * phrases.length);
+    return phrases[randomIndex];
+  }
+
+  useEffect(() => {
+    setLoadingPhrase(getRandomLoadingPhrase());
+  }, []);
+
   return (
     <div className="grid h-svh w-svw place-items-center">
       <div className="grid place-items-center">
-        <LoaderPinwheel size={48} className="animate-spin" />
-        <h1 className="text-3xl">EZ Rest</h1>
+        <div className="flex items-center gap-2">
+          <Coffee size={28} />
+          <h1 className="text-3xl">EZRest</h1>
+        </div>
+        <p className="h-4 animate-in fade-in">
+          {loadingPhrase ?? "Initializing..."}
+        </p>
       </div>
     </div>
   );
