9f5bf8a6f3750d27d608900c1897e20713953db6
Samuel Mårtensson <samuel137@live.se>
2025-01-07 21:50:57 +0100

    .

--- frontend/src/components/LanguagePicker.tsx
+++ frontend/src/components/LanguagePicker.tsx
@@ -1,7 +1,7 @@
 "use client";
 
+import { ChevronDown, Languages } from "lucide-react";
 import * as React from "react";
-import { Check, ChevronDown, Languages } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import {
