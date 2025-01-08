392d10dcacffede876a3ab82e8b98018e9af9c58
Samuel Mårtensson <samuel137@live.se>
2025-01-08 23:06:01 +0100

    rm semicolon

--- frontend/src/app/menu/page.tsx
+++ frontend/src/app/menu/page.tsx
@@ -8,7 +8,7 @@ const MenuPage = async () => {
   if (!data?.menu) return null;
   return (
     <SubPageLayout title={data.siteTranslations?.menu}>
-      <MenuRender data={data.menu} currency={data?.currency ?? ""} />;
+      <MenuRender data={data.menu} currency={data?.currency ?? ""} />
       <title>{`${data.siteTranslations?.menu} | ${data?.siteName}`}</title>
     </SubPageLayout>
   );
