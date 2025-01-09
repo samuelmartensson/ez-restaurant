8457489fae2f3379f731a4a8595f01438a5fdd60
Samuel Mårtensson <samuel137@live.se>
2025-01-09 20:34:59 +0100

    x

--- admin/src/app/api/scrape-theme/route.ts
+++ admin/src/app/api/scrape-theme/route.ts
@@ -4,56 +4,50 @@ import puppeteer from "puppeteer-core";
 
 export const maxDuration = 20;
 export async function GET(request: NextRequest) {
-  try {
-    const isLocal = !!process.env.CHROME_EXECUTABLE_PATH;
-    const color = request.nextUrl.searchParams.get("color") ?? "";
-    console.log(request.nextUrl.searchParams.get("color"));
+  const isLocal = !!process.env.CHROME_EXECUTABLE_PATH;
+  const color = request.nextUrl.searchParams.get("color") ?? "";
+  console.log(request.nextUrl.searchParams.get("color"));
 
-    const browser = await puppeteer.launch({
-      args: isLocal ? puppeteer.defaultArgs() : chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath:
-        process.env.CHROME_EXECUTABLE_PATH ||
-        (await chromium.executablePath(
-          "https://ez-rest.s3.eu-north-1.amazonaws.com/scraper/chromium-v126.0.0-pack.tar",
-        )),
-      headless: chromium.headless,
-    });
+  const browser = await puppeteer.launch({
+    args: isLocal ? puppeteer.defaultArgs() : chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath:
+      process.env.CHROME_EXECUTABLE_PATH ||
+      (await chromium.executablePath(
+        "https://ez-rest.s3.eu-north-1.amazonaws.com/scraper/chromium-v126.0.0-pack.tar",
+      )),
+    headless: chromium.headless,
+  });
 
-    const siteUrl = "https://zippystarter.com/tools/shadcn-ui-theme-generator";
+  const siteUrl = "https://zippystarter.com/tools/shadcn-ui-theme-generator";
 
-    const context = browser.defaultBrowserContext();
-    const page = await browser.newPage();
+  const context = browser.defaultBrowserContext();
+  const page = await browser.newPage();
 
-    await context.overridePermissions("https://zippystarter.com", [
-      "clipboard-read",
-      "clipboard-sanitized-write",
-      "clipboard-write",
-    ]);
+  await context.overridePermissions("https://zippystarter.com", [
+    "clipboard-read",
+    "clipboard-sanitized-write",
+    "clipboard-write",
+  ]);
 
-    page.setDefaultTimeout(2000);
-    await page.goto(siteUrl);
-    const input = await page.$("#color-text");
-    await input?.type("#" + color);
-    await page.click(
-      "xpath//html/body/div[1]/div/div/div/div[2]/div[2]/div/div/div[2]/div[3]/button[2]",
-    );
-    const dialogElement = await page.$('[role="dialog"]');
-    const copyBtn = await dialogElement?.$("button");
-    await copyBtn?.click();
+  page.setDefaultTimeout(2000);
+  await page.goto(siteUrl);
+  const input = await page.$("#color-text");
+  await input?.type("#" + color);
+  await page.click(
+    "xpath//html/body/div[1]/div/div/div/div[2]/div[2]/div/div/div[2]/div[3]/button[2]",
+  );
+  const dialogElement = await page.$('[role="dialog"]');
+  const copyBtn = await dialogElement?.$("button");
+  await copyBtn?.click();
 
-    const clipboardText = await page.evaluate(() =>
-      navigator.clipboard.readText(),
-    );
+  const clipboardText = await page.evaluate(() =>
+    navigator.clipboard.readText(),
+  );
 
-    await browser.close();
+  await browser.close();
 
-    return Response.json({
-      theme: clipboardText,
-    });
-  } catch (e) {
-    return Response.json({
-      e: JSON.stringify(e),
-    });
-  }
+  return Response.json({
+    theme: clipboardText,
+  });
 }
