308ecd456361dcdf8fc60ed8544993651fbee356
Samuel Mårtensson <samuel137@live.se>
2025-02-15 17:32:42 +0100

    analytics loading

--- admin/src/app/analytics/page.tsx
+++ admin/src/app/analytics/page.tsx
@@ -3,6 +3,7 @@
 import { useDataContext } from "@/components/DataContextProvider";
 import hasDomain from "@/components/hasDomain";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
 import { useGetCustomerAnalytics } from "@/generated/endpoints";
 import { cn } from "@/lib/utils";
 
@@ -10,10 +11,12 @@ const StatisticCard = ({
   title,
   value,
   previousValue,
+  isLoading,
 }: {
   title: string;
   value: number;
   previousValue: number;
+  isLoading: boolean;
 }) => {
   const periodDiff = value - previousValue;
   const isGain = periodDiff >= 0;
@@ -24,18 +27,26 @@ const StatisticCard = ({
         <CardTitle className="text-sm font-normal">{title}</CardTitle>
       </CardHeader>
       <CardContent>
-        <div className="mb-4 text-2xl font-bold">{value}</div>
+        {isLoading ? (
+          <Skeleton className="mb-4 h-8 w-20 rounded" />
+        ) : (
+          <div className="mb-4 text-2xl font-bold">{value}</div>
+        )}
         <p className="grid gap-0.5 text-xs text-muted-foreground">
           <span>Last 30 days</span>
-          <span
-            className={cn(
-              "font-bold",
-              isGain ? "text-green-700" : "text-red-700",
-            )}
-          >
-            {isGain ? "+" : ""}
-            {periodDiff}
-          </span>
+          {isLoading ? (
+            <Skeleton className="h-4 w-8 rounded" />
+          ) : (
+            <span
+              className={cn(
+                "font-bold",
+                isGain ? "text-green-700" : "text-red-700",
+              )}
+            >
+              {isGain ? "+" : ""}
+              {periodDiff}
+            </span>
+          )}
         </p>
       </CardContent>
     </Card>
@@ -44,23 +55,26 @@ const StatisticCard = ({
 
 const Hero = () => {
   const { selectedDomain } = useDataContext();
-  const { data } = useGetCustomerAnalytics({
+  const { data, isLoading } = useGetCustomerAnalytics({
     key: selectedDomain,
   });
 
   return (
     <div className="grid gap-2 md:grid-cols-3">
       <StatisticCard
+        isLoading={isLoading}
         title="Website visitors"
         value={Number(data?.current?.sessions ?? 0)}
         previousValue={Number(data?.previous?.sessions ?? 0)}
       />
       <StatisticCard
+        isLoading={isLoading}
         title="New visitors"
         value={Number(data?.current?.newUsers ?? 0)}
         previousValue={Number(data?.previous?.newUsers ?? 0)}
       />
       <StatisticCard
+        isLoading={isLoading}
         title="Menu visitors"
         value={Number(data?.currentMenu?.sessions ?? 0)}
         previousValue={Number(data?.previousMenu?.sessions ?? 0)}
